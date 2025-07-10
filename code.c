786d7a9ce24724420747eff92a37ba9aa9a09f3b
samformation <78386381+samformation@users.noreply.github.com>
2025-07-10 16:25:23 +0200

    Update code.c

--- code.c
+++ code.c
@@ -2,7 +2,17 @@
 
 int main(void)
 {
-  printf("code.c");
+  unsigned int x[32];
+  unsigned int s = 0;
+
+  for (s = 0; s < 32; s++) {
+    unsigned int *p = x;
+
+    *p = 1;
+    printf("x[%u] = %u\n", s, *p);
+    p++;
+  }
   
+  printf("code.c\n");
   return 0;
 }
